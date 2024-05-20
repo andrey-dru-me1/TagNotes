356deb758a2a13bb28f37cbd5c8e97017c1beb01
andrey-dru-me1 <andrey260503@gmail.com>
2024-05-21 00:43:06 +0700

    1.1.2 - UserController.php - Add User controller

---
+++ backend/app/src/Controller/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/api/user/{id}', name: 'get_user', methods: ['GET'])]
+    public function getUserById(int $id, UserRepository $userRepository): JsonResponse
+    {
+        return $this->json($userRepository->find($id));
+    }
+
+    #[Route('/api/users', name: 'list_users', methods: ['GET'])]
+    public function getUsers(UserRepository $userRepository): JsonResponse
+    {
+        return $this->json($userRepository->findAll());
+    }
+
+    #[Route('/api/user', name: 'post_user', methods: ['POST'])]
+    public function createUser(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        try {
+            $data = json_decode($request->getContent(), true);
+            $request->request->replace($data);
+
+            $user = new User();
+            $user->setName($request->get('name'));
+            $user->setPasswordHash($request->get('password'));
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $data = [
+                'status' => 200,
+                'success' => "Post added successfully",
+            ];
+            return new JsonResponse($data);
+        } catch (\Exception $e){
+            $data = [
+                'status' => 422,
+                'errors' => "$e",
+            ];
+            return new JsonResponse($data, 422);
+        }
+
+    }
+}
