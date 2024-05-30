5896beb5c95fe0304a40016db54f247184bbc801
andrey-dru-me1 <andrey260503@gmail.com>
2024-05-30 18:20:08 +0700

    1.3.1 - `NoteController.php` - Create API endpoints for note deletion and tag (un)pinning

--- backend/app/src/Controller/NoteController.php
+++ backend/app/src/Controller/NoteController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Note;
+use App\Entity\NoteTag;
 use App\Repository\NoteRepository;
+use App\Repository\NoteTagRepository;
+use App\Repository\TagRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,6 +16,71 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class NoteController extends AbstractController
 {
+
+    #[Route('/api/note/{noteId}/tag/{tagId}', name: 'remove_tag', methods: ['DELETE'])]
+    public function removeTag(
+        int $noteId,
+        int $tagId,
+        NoteRepository $noteRepository,
+        TagRepository $tagRepository,
+        NoteTagRepository $noteTagRepository,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+        $note = $noteRepository->find($noteId);
+        $tag = $tagRepository->find($tagId);
+        $noteTags = $noteTagRepository->findBy(['note' => $note, 'tag' => $tag]);
+        foreach ($noteTags as $noteTag) {
+            $entityManager->remove($noteTag);
+        }
+        $entityManager->flush();
+
+        $data = [
+            'status' => Response::HTTP_OK,
+            'success' => 'Tag is successfully unpinned from note',
+        ];
+        return new JsonResponse($data, $data['status']);
+    }
+
+    #[Route('/api/note/{noteId}/tag/{tagId}', name: 'append_tag', methods: ['POST'])]
+    public function appendTag(
+        int $noteId,
+        int $tagId,
+        NoteRepository $noteRepository,
+        TagRepository $tagRepository,
+        NoteTagRepository $noteTagRepository,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+        $note = $noteRepository->find($noteId);
+        $tag = $tagRepository->find($tagId);
+        if ($noteTagRepository->count(['note' => $note, 'tag' => $tag]) == 0) {
+            $noteTag = new NoteTag();
+            $noteTag->setNote($note);
+            $noteTag->setTag($tag);
+            $entityManager->persist($noteTag);
+            $entityManager->flush();
+
+            $data = [
+                'status' => Response::HTTP_OK,
+                'success' => 'Tag is successfully added to note',
+            ];
+        } else {
+            $data = [
+                'status' => Response::HTTP_BAD_REQUEST,
+                'error' => 'Relation has already existed',
+            ];
+        }
+        return new JsonResponse($data, $data['status']);
+    }
+
+    #[Route('/api/note/{id}/tags', name: 'list_note_tags', methods: ['GET'])]
+    public function listTags(int $id, NoteTagRepository $noteTagRepository): JsonResponse
+    {
+        $noteTags = $noteTagRepository->findBy(['note' => $id]);
+        $tags = array_map(function (NoteTag $noteTag) {
+            return $noteTag->getTag();
+        }, $noteTags);
+        return $this->json($tags, Response::HTTP_OK);
+    }
 
     #[Route('/api/notes', name: 'list_notes', methods: ['GET'])]
     public function listNotes(NoteRepository $noteRepository): JsonResponse
@@ -24,6 +92,20 @@ class NoteController extends AbstractController
     public function getNote(int $id, NoteRepository $noteRepository): JsonResponse
     {
         return $this->json($noteRepository->find($id), Response::HTTP_OK);
+    }
+
+    #[Route('/api/note/{id}', name: 'delete_note', methods: ['DELETE'])]
+    public function deleteNote(int $id, NoteRepository $noteRepository, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $note = $noteRepository->find($id);
+        $entityManager->remove($note);
+        $entityManager->flush();
+
+        $data = [
+            'status' => Response::HTTP_OK,
+            'success' => 'Note is successfully deleted',
+        ];
+        return new JsonResponse($data, $data['status']);
     }
 
     #[Route('/api/note/{id}', name: 'edit_note', methods: ['POST'])]
