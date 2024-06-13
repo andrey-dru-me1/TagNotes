268d8b45131fc26746adebdad298af99d8ab3015
andrey-dru-me1 <andrey260503@gmail.com>
2024-06-13 22:43:05 +0700

    2.1.0 - `StoreProvider.tsx` - Rewrite `StoreProvider.tsx` file from scratch

--- frontend/app/StoreProvider.tsx
+++ frontend/app/StoreProvider.tsx
@@ -1,31 +1,14 @@
-"use client";
-import type { AppStore } from "@/lib/store";
-import { makeStore } from "@/lib/store";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import type { ReactNode } from "react";
-import { useEffect, useRef } from "react";
-import { Provider } from "react-redux";
+'use client'
 
-interface Props {
-  readonly children: ReactNode;
-}
+import {ReactNode, useRef} from 'react'
+import {Provider} from 'react-redux'
+import {makeStore, AppStore} from '@/lib/store'
 
-export const StoreProvider = ({ children }: Props) => {
-  const storeRef = useRef<AppStore | null>(null);
-
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore();
-  }
-
-  useEffect(() => {
-    if (storeRef.current != null) {
-      // configure listeners using the provided defaults
-      // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
-      const unsubscribe = setupListeners(storeRef.current.dispatch);
-      return unsubscribe;
+export default function StoreProvider({children}: { children: ReactNode }) {
+    const storeRef = useRef<AppStore>()
+    if (!storeRef.current) {
+        storeRef.current = makeStore()
     }
-  }, []);
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
-};
+    return <Provider store={storeRef.current}>{children}</Provider>
+}
