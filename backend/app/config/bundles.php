ad108c5ea690375a4e433b61878cf6eae2347fdc
andrey-dru-me1 <andrey260503@gmail.com>
2024-06-13 22:38:02 +0700

    2.1.0 - `bundles.php` - Add easyAdmin bundle

--- backend/app/config/bundles.php
+++ backend/app/config/bundles.php
@@ -17,4 +17,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
