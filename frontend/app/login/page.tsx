4f6f6871abd1555a287f196288ebeba69e70a2bc
andrey-dru-me1 <andrey260503@gmail.com>
2024-06-14 20:47:27 +0700

    2.3.0 - `/` - Restructure the whole project

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,13 +1,31 @@
+'use client'
+
 import {Box, TextField} from "@mui/material";
 import {Stack} from "@mui/material";
 import {Button} from "@mui/material";
+import {useState} from "react";
+import axios from "axios";
 
 export default function LoginPage() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleClick = () => {
+        axios({
+            method: "post",
+            url: "http://tagnotes/api/login",
+            data: {"name": username, "password": password},
+            headers: {"Content-Type": "application/json"},
+        }).then((response => console.log(response))).catch(e => console.log(e));
+    }
+
     return <Box alignItems="center" justifyContent="center">
         <Stack spacing={2} alignItems="center">
-            <TextField label="Username" type={"text"} placeholder="Username" variant="outlined" required={true}/>
-            <TextField label="Password" type={"password"} placeholder="Password" variant="outlined" required={true} />
-            <Button variant="contained" color="primary" type="submit">Login</Button>
+            <TextField label="Username" type={"text"} placeholder="Username" variant="outlined" required={true}
+                       onChange={(t) => setUsername(t.target.value)}/>
+            <TextField label="Password" type={"password"} placeholder="Password" variant="outlined" required={true}
+                       onChange={(t) => setPassword(t.target.value)}/>
+            <Button variant="contained" color="primary" type="submit" onClick={handleClick}>Login</Button>
         </Stack>
     </Box>
 }
