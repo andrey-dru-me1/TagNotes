4c1cbb387b1818dfdd7e1ebeb0e5ac0598de11e2
andrey-dru-me1 <andrey260503@gmail.com>
2024-06-26 21:01:21 +0700

    2.2.1 - `NoteTagRepository.php` - Add new query to get all note ids that have all the given tags

--- backend/src/Repository/NoteTagRepository.php
+++ backend/src/Repository/NoteTagRepository.php
@@ -18,40 +18,56 @@ class NoteTagRepository extends ServiceEntityRepository
         parent::__construct($registry, NoteTag::class);
     }
 
-//    /**
-//     * @return NoteTag[] Returns an array of NoteTag objects
-//     */
-//    public function findByNote(Note $note): array
-//    {
-//        return $this->createQueryBuilder('n')
-//            ->andWhere('n.note = :val')
-//            ->setParameter('val', $note)
-//            ->orderBy('n.id', 'ASC')
-//            ->getQuery()
-//            ->getResult();
-//    }
-//
-//    /**
-//     * @return NoteTag[] Returns an array of NoteTag objects
-//     */
-//    public function findByTag(Tag $tag): array
-//    {
-//        return $this->createQueryBuilder('n')
-//            ->andWhere('n.note = :val')
-//            ->setParameter('val', $tag)
-//            ->orderBy('n.id', 'ASC')
-//            ->getQuery()
-//            ->getResult();
-//    }
-//
-//    public function findByNoteAndTag(Note $note, Tag $tag): array
-//    {
-//        return $this->createQueryBuilder('n')
-//            ->andWhere('n.note = :note')
-//            ->andWhere('n.tag = :tag')
-//            ->setParameter('note', $note)
-//            ->setParameter('tag', $tag)
-//            ->getQuery()
-//            ->getResult();
-//    }
+    public function findNoteIdsByTagIds(array $tags)
+    {
+        $tagCount = count($tags);
+        return $this->createQueryBuilder('n')
+            ->select('IDENTITY(n.note)')
+            // ->from('note_tag', 'nt')
+            ->where('n.tag IN (:tags)')
+            ->setParameter('tags', $tags)
+            ->groupBy('n.note')
+            ->having('COUNT(DISTINCT n.tag) = :tagCount')
+            ->setParameter('tagCount', $tagCount)
+            ->distinct()
+            ->getQuery()
+            ->getResult();
+    }
+
+    //    /**
+    //     * @return NoteTag[] Returns an array of NoteTag objects
+    //     */
+    //    public function findByNote(Note $note): array
+    //    {
+    //        return $this->createQueryBuilder('n')
+    //            ->andWhere('n.note = :val')
+    //            ->setParameter('val', $note)
+    //            ->orderBy('n.id', 'ASC')
+    //            ->getQuery()
+    //            ->getResult();
+    //    }
+    //
+    //    /**
+    //     * @return NoteTag[] Returns an array of NoteTag objects
+    //     */
+    //    public function findByTag(Tag $tag): array
+    //    {
+    //        return $this->createQueryBuilder('n')
+    //            ->andWhere('n.note = :val')
+    //            ->setParameter('val', $tag)
+    //            ->orderBy('n.id', 'ASC')
+    //            ->getQuery()
+    //            ->getResult();
+    //    }
+    //
+    //    public function findByNoteAndTag(Note $note, Tag $tag): array
+    //    {
+    //        return $this->createQueryBuilder('n')
+    //            ->andWhere('n.note = :note')
+    //            ->andWhere('n.tag = :tag')
+    //            ->setParameter('note', $note)
+    //            ->setParameter('tag', $tag)
+    //            ->getQuery()
+    //            ->getResult();
+    //    }
 }
