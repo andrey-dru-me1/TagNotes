25dc33eb20f984ca132089bad53f99b12b3b39dc
andrey-dru-me1 <andrey260503@gmail.com>
2024-06-27 18:03:29 +0700

    2.2.1 - `NoteElement.tsx` - Modify styles for NoteElement

--- frontend/lib/features/MyNotes/notes/oneNoteView/NoteElement.tsx
+++ frontend/lib/features/MyNotes/notes/oneNoteView/NoteElement.tsx
@@ -1,22 +1,32 @@
 import { onDeleteButtonClick } from "@/lib/features/MyNotes/notes/buttonHandler";
 import Note from "@/lib/types/Note";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Stack } from "@mui/material";
 import Link from "next/link";
 
 export default function NoteElement(props: { note: Note }) {
   const { note } = props;
   return (
-    <Box key={note.id} width={500}>
-      <Box fontSize={26} sx={{ fontWeight: "bold" }}>
-        {note.title}
-        <Link href={`/note/${props.note.id}`}> Edit</Link>
-        <Button
-          size={"small"}
-          onClick={() => onDeleteButtonClick(props.note.id)}
-        >
-          Del
-        </Button>
-      </Box>
+    <Box key={note.id} marginBottom={3}>
+      <Stack
+        direction={"row"}
+        marginBottom={2}
+        justifyContent={"space-between"}
+      >
+        <Box fontSize={32} sx={{ fontWeight: "bold" }}>
+          {note.title}
+        </Box>
+        <Stack direction={"row"}>
+          <Box display={"flex"} alignItems={"center"}>
+            <Link href={`/note/${props.note.id}`}> Edit</Link>
+          </Box>
+          <Button
+            size={"small"}
+            onClick={() => onDeleteButtonClick(props.note.id)}
+          >
+            Del
+          </Button>
+        </Stack>
+      </Stack>
       <Box whiteSpace={"pre-line"}>{note.content}</Box>
     </Box>
   );
