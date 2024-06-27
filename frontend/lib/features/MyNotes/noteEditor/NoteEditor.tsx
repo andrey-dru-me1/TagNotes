dbc38897af84ff1e87282946d360d46728dac8b2
andrey-dru-me1 <andrey260503@gmail.com>
2024-06-27 17:17:25 +0700

    2.2.1 - `Notes.tsx` - Add styles for NoteEditor component

--- frontend/lib/features/MyNotes/noteEditor/NoteEditor.tsx
+++ frontend/lib/features/MyNotes/noteEditor/NoteEditor.tsx
@@ -51,22 +51,39 @@ export default function NoteEditor({ noteId }: { noteId: number }) {
         direction={"row"}
         sx={{ borderRadius: 8, border: 4 }}
       >
-        <Box width={700} margin={2}>
+        <Stack direction={"column"} width={700} margin={1}>
           <TextField
             fullWidth
+            variant="standard"
             defaultValue={note?.title}
             onChange={onTitleChange}
+            InputProps={{
+              disableUnderline: true,
+              sx: { fontSize: 32, fontWeight: "bold" },
+            }}
           />
           <TextField
             multiline
+            variant="standard"
             fullWidth
-            defaultValue={note?.content}
+            value={note?.content}
             onChange={onContentChange}
+            sx={{ height: "100%" }}
+            InputProps={{
+              disableUnderline: true,
+              sx: { height: "100%", alignItems: "start" },
+            }}
           />
-          <Button variant={"outlined"} onClick={onSaveClick}>
-            Save
-          </Button>
-        </Box>
+          <Box>
+            <Button
+              sx={{ size: "fit-content" }}
+              variant={"outlined"}
+              onClick={onSaveClick}
+            >
+              Save
+            </Button>
+          </Box>
+        </Stack>
         <Divider orientation="vertical" flexItem />
         {note !== null && <TagAppender note={note} />}
       </Stack>
