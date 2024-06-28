909274e17e13169ef66681483433c10565aaf566
andrey-dru-me1 <andrey260503@gmail.com>
2024-06-28 17:32:05 +0700

    2.5.1 - `DashboardController.php` - Modify DashboardController to manage db tables

--- backend/src/Controller/Admin/DashboardController.php
+++ backend/src/Controller/Admin/DashboardController.php
@@ -2,45 +2,43 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\Tag;
+use App\Entity\Note;
+use App\Entity\User;
+use App\Entity\NoteTag;
+use App\Entity\RefreshToken;
+use App\Entity\NoteAccessLog;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+    #[Route('/api/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(UserCrudController::class)->generateUrl();
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-//         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-//         return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-//         return $this->render('some/path/my-dashboard.html.twig');
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('App');
+            ->setTitle('TagNotes');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Users', 'fas fa-map-marker-alt', User::class);
+        yield MenuItem::linkToCrud('Notes', 'fas fa-map-marker-alt', Note::class);
+        yield MenuItem::linkToCrud('Tags', 'fas fa-map-marker-alt', Tag::class);
+        yield MenuItem::linkToCrud('NoteTags', 'fas fa-map-marker-alt', NoteTag::class);
+        yield MenuItem::linkToCrud('RefreshTokens', 'fas fa-map-marker-alt', RefreshToken::class);
+        yield MenuItem::linkToCrud('NoteAccessLogs', 'fas fa-map-marker-alt', NoteAccessLog::class);
     }
 }
