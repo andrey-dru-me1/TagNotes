bde5baf13cbe1c49ef60c7805b8de52fdd8f768a
andrey-dru-me1 <andrey260503@gmail.com>
2024-07-03 17:52:58 +0700

    2.6.0 - `NoteAccessLogRepository.php` - Add old notes returning at sort method

--- backend/src/Repository/NoteAccessLogRepository.php
+++ backend/src/Repository/NoteAccessLogRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\NoteAccessLog;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
@@ -11,8 +12,12 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
  */
 class NoteAccessLogRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+
+    private EntityManagerInterface $em;
+
+    public function __construct(ManagerRegistry $registry, EntityManagerInterface $em)
     {
+        $this->em = $em;
         parent::__construct($registry, NoteAccessLog::class);
     }
 
@@ -30,18 +35,36 @@ class NoteAccessLogRepository extends ServiceEntityRepository
         }
         foreach ($noteAccessLogs as $noteAccessLog) {
             $noteId = $noteAccessLog->getNote()->getId();
-            $logTime = $noteAccessLog->getAccessDate()->getTimestamp();
+            $logDate = $noteAccessLog->getAccessDate();
+            $logTime = $logDate->getTimestamp();
             if (!array_key_exists($noteId, $notePoints)) {
                 $notePoints[$noteId] = 0.;
             }
             $diffTime = $currentTime - $logTime;
-            $notePoints[$noteId] += 1. / $diffTime;
+            $diff = exp(-$diffTime / 1e5);
+            if ($diff < 1e-9) {
+                $this->em->remove($noteAccessLog);
+            } else {
+                $notePoints[$noteId] += $diff;
+            }
         }
+        $this->em->flush();
         arsort($notePoints, SORT_NUMERIC);
+
+        $oldNotePoints = array_filter($notePoints, function ($point) {
+            return $point < 1e-2;
+        });
+        $oldNoteIds = array_keys($oldNotePoints);
+        $oldNotes = array_map(function (int $noteId) use ($idToNote) {
+            return $idToNote[$noteId];
+        }, $oldNoteIds);
+
         $sortedNoteIds = array_keys($notePoints);
         $sortedNotes = array_map(function (int $noteId) use ($idToNote) {
             return $idToNote[$noteId];
         }, $sortedNoteIds);
-        return $sortedNotes;
+
+        $result = ['sorted' => $sortedNotes, 'old' => $oldNotes];
+        return $result;
     }
 }
