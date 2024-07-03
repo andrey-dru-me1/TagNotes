16dd84003150aeed0345c6e2f80662607fc12bdb
andrey-dru-me1 <andrey260503@gmail.com>
2024-07-03 17:50:52 +0700

    2.6.0 - `NoteAccessLogCrudController.php` - Configure fields manually to add an explicit note relation field

--- backend/src/Controller/Admin/NoteAccessLogCrudController.php
+++ backend/src/Controller/Admin/NoteAccessLogCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\NoteAccessLog;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class NoteAccessLogCrudController extends AbstractCrudController
@@ -10,5 +13,14 @@ class NoteAccessLogCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return NoteAccessLog::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            AssociationField::new('note')->onlyOnIndex(),
+            DateTimeField::new('access_date'),
+        ];
     }
 }
